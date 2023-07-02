81f6379b79c47305b929bc01edccbecb424532f3
kssusweb <kssus7@gmail.com>
2023-07-02 14:55:55 +0900

    compornent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
+function Header(props){
+  console.log('props', props);
+  return <header>
+      <h1><a href="/">{props.title}</a></h1>
+    </header>
+}
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/html/'+t.id}>{t.title}</a></li>)
+  }
+  return <nav>
+      <ol>
+        {lis}
+      </ol>
+     </nav>
+}
+function Article(props){
+  return <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+
+}
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'react', body:'react is ...'}
+]
   return (
     <div className="App">
-     Hello world!
+     <Header title="WEB"></Header>
+     <Nav topics={topics}></Nav>
+     <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
